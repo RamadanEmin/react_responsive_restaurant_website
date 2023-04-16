e85714fa51fef9786f97ef9563eb396773a41858
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-16 21:40:35 +0300

    Styling of Laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { SubHeading } from '../../components';
 import { images, data } from '../../constants';
+import './Laurels.css';
 
 const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
     <div className="app__laurels_awards-card">
