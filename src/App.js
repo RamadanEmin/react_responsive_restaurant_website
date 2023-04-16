9c3633f4f74bbf429a16da8418bd2cc32f0881dd
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-16 10:21:15 +0300

    Added "Chef" section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { AboutUs, Header, SpecialMenu } from './container';
+import { AboutUs, Chef, Header, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
 
@@ -10,6 +10,7 @@ const App = () => (
     <Header />
     <AboutUs />
     <SpecialMenu />
+    <Chef />
   </div>
 );
 
--- src/container/index.js
+++ src/container/index.js
@@ -1,9 +1,11 @@
 import AboutUs from './AboutUs/AboutUs';
+import Chef from './Chef/Chef';
 import Header from './Header/Header';
 import SpecialMenu from './Menu/SpecialMenu';
 
 export {
   AboutUs,
+  Chef,
   Header,
-  SpecialMenu
+  SpecialMenu,
 };
