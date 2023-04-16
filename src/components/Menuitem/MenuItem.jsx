1ece296a8f52d09fa47f646f515cb99700d4062e
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-16 09:34:43 +0300

    Styling of "MenuItem"

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './MenuItem.css';
+
 const MenuItem = ({ title, price, tags }) => (
     <div className="app__menuitem">
         <div className="app__menuitem-head">
