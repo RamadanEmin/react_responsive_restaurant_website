19f2a6da5220a4774b29ea9e4f9a004b6d408243
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-04-16 11:02:17 +0300

    Styling of "Chef"

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
+import './Chef.css';
 
 const Chef = () => (
     <div className="app__bg app__wrapper section__padding">
